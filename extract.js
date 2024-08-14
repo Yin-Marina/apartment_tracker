a0046f53e07127703e08d71b81ccede248402cb5
MutaoYin <72797020+Yin-Marina@users.noreply.github.com>
2024-08-14 14:21:10 -0400

    Update and rearrange the project structure

--- extract.js
+++
@@ -1,6 +0,0 @@
-const propertyElement = document.querySelector('.info-window.for-property');
-const propertyName = propertyElement.querySelector('h4').innerText;
-const propertyAddress = propertyElement.querySelector('.building-info p').innerText;
-const propertyPhone = propertyElement.querySelector('.building-info .phone').innerText;
-
-console.log(propertyName, propertyAddress, propertyPhone);
